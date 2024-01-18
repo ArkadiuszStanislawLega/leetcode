70525a0a8a1da4736e6ac5be28f91deb2f82fd4f
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-01-18 07:10:00 +0100

    70 - daily 18.01.24

---
+++ 70/main.cpp
@@ -0,0 +1,92 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+class Solution {
+	private: 
+	int memoization(int n, vector<int>& v){
+		if(n <= 1){
+			return 1;
+		}
+
+		if(v[n] != -1){
+			return v[n];
+		}
+
+		return v[n] = memoization(n-1, v) + memoization(n-2, v);
+	}
+
+	public:
+	int climbingStairs(int n){
+		vector<int> counter (n+1, -1);
+		return memoization(n, counter);
+	}
+};
+
+void print_vector(vector<int>& v){
+	cout << '{';
+	for(const int &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+
+	vector<int> tests = {
+		2,
+		3
+	};
+
+	vector<int> answers = {
+		2,
+		3
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->climbingStairs(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
