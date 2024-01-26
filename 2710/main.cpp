497555ba11afcff75eadaf69b658ee17baf2b982
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-01-26 13:24:16 +0100

    2710 - accepted

---
+++ 2710/main.cpp
@@ -0,0 +1,84 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+class Solution {
+	public:
+	string removeTrailingZeros(string num){
+		for(int i = num.size()-1; i >= 0; i--){
+			if(num[i] != '0'){
+				return num.substr(0, i+1);
+			}
+		}
+		
+		return num;
+	}
+};
+
+void print_vector(vector<int>& v){
+	cout << '{';
+	for(const int &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	string answer {};
+	double elapsed_time {};
+
+	vector<string> tests = {
+		"51230100",
+		"123"
+	};
+
+	vector<string> answers = {
+		"512301",
+		"123"
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->removeTrailingZeros(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
