11cecaa22d35c91cae969b133b3bce90d9192718
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-02-07 19:53:54 +0100

    451 - daily 07.02.24

---
+++ 451/main.cpp
@@ -0,0 +1,98 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+class Solution {
+	public:
+	string frequencySort(string s){
+		string answer {};
+		vector<pair<char, int>> counter (74);
+		for(const char& c : s){
+			counter[c-'0'].first = c;
+			counter[c-'0'].second++;
+		}
+		sort(counter.begin(), counter.end(), [](pair<char,int> a, pair<char,int> b){ return a.second > b.second;});
+		for(pair<char, int>& i : counter){
+			if(i.second == 0){
+				break;
+			}
+			
+			while(i.second){
+				answer += i.first;
+				i.second--;
+			}
+		}
+		return answer;
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	string answer {};
+	double elapsed_time {};
+
+	vector<string> tests = {
+		"tree",
+		"cccaaa",
+		"Aabb"
+	};
+
+	vector<string> answers = {
+		"eetr",
+		"cccaaa",
+		"bbAa"
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->frequencySort(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
