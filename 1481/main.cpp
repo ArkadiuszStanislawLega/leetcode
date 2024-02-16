5012afd4f069ae9582e7de509803613eacf0304a
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-02-16 10:04:19 +0100

    1481 - daily 16.02.24

---
+++ 1481/main.cpp
@@ -0,0 +1,102 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+class Solution {
+	public:
+	int findLeastNumOfUniqueInts(vector<int>& arr, int k){
+		int temp {};
+		map<int, int> container {};
+		vector<int> freq {};
+
+		for(const int& i : arr){
+			container[i]++;
+		}
+
+		for(auto &i : container){
+			freq.push_back(i.second);
+		}
+
+		sort(freq.begin(), freq.end());
+
+		for(int i {}; i < freq.size(); i++){
+			temp += freq[i];
+			if(temp > k){
+				return freq.size()-i;
+			}
+		}
+
+
+		return 0;
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+
+	vector<pair<vector<int>, int>> tests = {
+		{{5,5,4}, 1},
+		{{4,3,1,1,3,3,2}, 3},
+	};
+
+	vector<int> answers = {
+		1,
+		2
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->findLeastNumOfUniqueInts(tests[i].first, tests[i].second);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
