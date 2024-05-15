42d32a63bbc6c3e3c7971619a6ceb5f9d0c5cd44
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-05-15 09:05:00 +0200

    1476

---
+++ 1476/main.cpp
@@ -0,0 +1,95 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class SubractangleQueries {
+private:
+	vector<vector<int>> _ractangle;
+public:
+	SubractangleQueries(vector<vector<int>>& ractangle): _ractangle(std::move(ractangle)){}
+
+	void updateSubractangle(int row1, int col1, int row2, int col2, int newValue){
+		for(int x {row1}; x <= row2; x++){
+			for(int y{col1}; y < col2; y++){
+				this->_ractangle[x][y] = newValue;
+			}
+		}
+	}
+
+	int getValue(int row, int col){
+		return this->_ractangle[row][col];
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+
+	vector<> tests = {
+	};
+
+	vector<> answers = {
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->SubractangleQueries(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
