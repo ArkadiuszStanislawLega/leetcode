ed392743f9293edcd5b75adc33cd47fd2e655b85
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-05-24 09:19:50 +0200

    807

---
+++ 807/main.cpp
@@ -0,0 +1,101 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+	public:
+	int maxIncreaseKeepingSkyLine(vector<vector<int>>& grid){
+		int answer {};
+		vector<int> maxRow (grid.size()), maxColumn(grid.size());
+		for(int i {}; i < grid.size(); i++){
+			for(int j{}; j < grid[i].size();j++){
+				maxRow[i] = max(maxRow[i], grid[i][j]);
+				maxColumn[j] = max(maxColumn[j], grid[i][j]);
+			}	
+		}
+		for(int i {}; i < grid.size(); i++){
+			for(int j{}; j < grid[i].size();j++){
+				answer += min(maxRow[i], maxColumn[j]) - grid[i][j];
+			}
+		}
+		
+		return answer;
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+
+	vector<vector<vector<int>>> tests = {
+		{{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}},
+		{{0,0,0},{0,0,0},{0,0,0}}
+	};
+
+	vector<int> answers = {
+		35,
+		0
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->maxIncreaseKeepingSkyLine(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
