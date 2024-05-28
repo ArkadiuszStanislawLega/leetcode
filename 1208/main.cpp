8c5566ce5c3fe4e0c32c6c0702fb63d762f8e8dc
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-05-28 10:13:20 +0200

    1208 - daily 28.05.24

---
+++ 1208/main.cpp
@@ -0,0 +1,111 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+	public:
+	int equalSubstring(string s, string t, int maxCost){
+		int answer {}, start {}, current_cost{};
+		for(int i {}; i < s.size(); i++){
+			current_cost += abs(s[i]-t[i]);
+			while(current_cost > answer){
+				current_cost -= abs(s[start]-t[start]);
+				start++;
+			}
+
+			answer = max(answer, i-start+1);
+		}
+		return answer;
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+
+	vector<pair<string, string>> tests = {
+		{"abcd", "bcdf"},
+		{"abcd","cdef"},
+		{"abcd", "acde"},
+		{"krrgw", "zjxss"},
+		{"pxezla", "loewbi"}
+	};
+
+	vector<int> test2 = {
+		3,
+		3,
+		0,
+		19,
+		25
+	};
+
+	vector<int> answers = {
+		3,
+		1,
+		1,
+		2,
+		4
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->equalSubstring(tests[i].first, tests[i].second, test2[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		//assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
