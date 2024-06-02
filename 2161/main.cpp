1f8f560e84bccd89b0f0ecc3fad23504b0b85428
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-06-02 08:39:00 +0200

    2161

---
+++ 2161/main.cpp
@@ -0,0 +1,105 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+	public:
+	vector<int> pivotArray(vector<int>& nums, int pivot){
+		vector<int> answer {}, before{}, equal{}, after{};
+		for(const int &i : nums){
+			if(i < pivot){
+				before.push_back(i);
+				continue;
+			}
+			if(i == pivot){
+				equal.push_back(i);
+				continue;
+			}
+			after.push_back(i);
+		}
+		answer.insert(answer.begin(), before.begin(), before.end());
+		answer.insert(answer.end(), equal.begin(), equal.end());
+		answer.insert(answer.end(), after.begin(), after.end());
+		return answer;
+	}
+};
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	vector<int> answer {};
+	double elapsed_time {};
+
+	vector<pair<vector<int>, int>> tests = {
+		{{9,12,5,10,14,3,10},10},
+		{{-3,4,3,2},2}
+	};
+
+	vector<vector<int>> answers = {
+		{9,5,3,10,10,12,14},
+		{-3,2,4,3}
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->pivotArray(tests[i].first, tests[i].second);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: ";
+		print_vector<int>(answers[i]);
+		cout << "\n\trecived value: ";
+		print_vector<int>(answer);
+		cout << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
