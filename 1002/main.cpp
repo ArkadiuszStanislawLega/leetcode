bd69c47ba478181070931d25ae11c2d660b67a46
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-06-05 11:49:16 +0200

    1002 - daily 05.06.24

---
+++ 1002/main.cpp
@@ -0,0 +1,117 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+#define LETTERS_IN_ALPHABET 26
+
+class Solution {
+	public:
+	vector<string> commonChars(vector<string>& words){
+		vector<string> answer {};
+		vector<int> whole_frequency (LETTERS_IN_ALPHABET), current_frequency (LETTERS_IN_ALPHABET);
+
+		for(const char &c : *words.begin()){
+			whole_frequency[c-'a']++;
+		}
+
+		for(int i {1}; i < words.size(); i++){
+			current_frequency = vector<int> (LETTERS_IN_ALPHABET);
+
+			for(const char &c : words[i]){
+				current_frequency[c-'a']++;
+			}
+
+			for(int j {}; j < LETTERS_IN_ALPHABET; j++){
+				whole_frequency[j] = min(whole_frequency[j], current_frequency[j]);
+			}
+		}
+
+		for(int i {}; i < LETTERS_IN_ALPHABET; i++){
+			for(int j {}; j < whole_frequency[i]; j++){
+				answer.push_back(string(1, i + 'a'));
+			}
+		}
+
+		return answer;
+	}
+};
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	vector<string> answer {};
+	double elapsed_time {};
+
+	vector<vector<string>> tests = {
+		{"bella","label", "roller"},
+		{"cool", "lock", "cook"}
+	};
+
+	vector<vector<string>> answers = {
+		{"e", "l", "l"},
+		{"c", "o"}
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->commonChars(tests[i]);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: ";
+		print_vector<string>(answers[i]);
+		cout << "\n\trecived value: ";
+		print_vector<string>(answer);
+		cout << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		//assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
