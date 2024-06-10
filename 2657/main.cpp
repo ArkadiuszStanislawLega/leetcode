692ba7e982bf745c539590beee497e6f8fdebc4a
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-06-10 10:57:12 +0200

    2657

---
+++ 2657/main.cpp
@@ -0,0 +1,101 @@
+#include <climits>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+	public:
+	vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B){
+		vector<int>answer;
+		int count {};
+
+		for(int i {}; i < A.size(); i++){
+			count = count_if(A.begin(), A.begin()+i+1, [&](int x){
+				return count_if(B.begin(), B.begin()+i+1, [&](int y ){
+					return x == y;} 
+				);
+			});
+			answer.push_back(count);
+		}
+		return answer;	
+	}
+};
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	vector<int> answer {};
+	double elapsed_time {};
+
+	vector<pair<vector<int>, vector<int>>> tests = {
+		{{1,3,2,4}, {3,1,2,4}},
+		{{2,3,1},{3,1,2}}
+	};
+
+	vector<vector<int>> answers = {
+		{0,2,3,4},
+		{0,1,3}
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->findThePrefixCommonArray(tests[i].first, tests[i].second);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: ";
+		print_vector<int>(answers[i]);
+		cout << "\n\trecived value: ";
+		print_vector<int>(answer);
+		cout << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		//assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
