8b317d3bf911d96e3ad0354181d4a70e464f76f6
Arkadiusz Łęga <arkadiusz.stanislaw.lega@gmail.com>
2024-06-23 12:13:44 +0200

    1438 - daily 23.06.24 - fail

---
+++ 1438/main.cpp
@@ -0,0 +1,107 @@
+#include <climits>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <ratio>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <map>
+#include <tgmath.h>
+#include <set>
+#include <stack>
+using namespace std;
+using namespace std::chrono;
+
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right){}
+};
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+	public:
+	int longestSubarray(vector<int> nums, int limit){
+		int answer {};
+		for(int size {1}; size <= nums.size(); size++){
+			for(int start {}; start < nums.size(); start++){
+				int end = start+size;
+				if(end <= nums.size()){
+					int minV {}, maxV{}, val {}; 
+					minV = *min_element(nums.begin()+start, nums.begin()+end);
+					maxV = *max_element(nums.begin()+start, nums.begin()+end);
+					val = abs(maxV-minV);
+
+					if (val <= limit){
+						answer = max(answer, size);
+					}
+				}
+			}
+		}
+
+		return answer;
+	}
+};
+
+template <typename T>
+void print_vector(vector<T>& v){
+	cout << '{';
+	for(const T &i: v){
+		cout << i << ',';
+	}
+	cout << "\b}";
+}
+
+int main (int argc, char *argv[]) {
+	int answer {};
+	double elapsed_time {};
+	
+	vector<pair<vector<int>, int>> tests = {
+		{{8,2,4,7}, 4},
+		{{10, 1,2,4,7,2},5},
+		{{4,2,2,2,4,4,2,2},0}
+	};
+
+	vector<int> answers = {
+		2,
+		4,
+		3
+	};
+
+	for(int i {}; i < tests.size(); i++){
+		Solution *s = new Solution();
+
+		auto start = high_resolution_clock::now();
+		answer = s->longestSubarray(tests[i].first, tests[i].second);
+		auto end = high_resolution_clock::now();
+
+		cout << "test " << i+1 << "\n\ttarget value: " << answers[i] << "\n\trecived value: " << answer << '\n';
+
+		elapsed_time = duration<double, milli>(end-start).count();
+		cout << "\nelapsed time " << elapsed_time << "ms";
+
+		assert(answer == answers.at(i));
+		cout << " -> passed\n";
+	}
+
+	//auto time_counter = duration_cast<microseconds> (time_start - time_start);
+	//cout << time_counter.count() << "ms " << sizeof(Solution) << '\n';
+
+	return 0;
+}
